6698737c6c8997013a566df8d79831b9d7ae9be0
Ian barbosa <147871831+Ianbarbosa1@users.noreply.github.com>
2024-06-25 21:39:49 -0300

    Update script.js
    alterandoa atualização não desejada do form

--- form/assents/js/script.js
+++ form/assents/js/script.js
@@ -1,4 +1,8 @@
-function enviar(){
+let envio = document.querySelector('#enviar') 
+    envio.addEventListener('click', enviar)
+
+function enviar(e){
+    /*Esta função permite que o formulário preenchido seja enviado para o whatsapp da empresa*/
     var telefone = "5521972106538"
 
     var n = document.getElementById('nome')
@@ -18,30 +22,33 @@ function enviar(){
     var tipoPlano = document.getElementById('plano')
     var optionValue = tipoPlano.options[tipoPlano.selectedIndex]
     var plano = optionValue.text
-
     
-
     if(nome === "" && genero === "Selecione" && idade === "" && numero === "" && plano === "Selecione"){
         alert("O formulário só será enviado se todos os campos estiverem completos!")
     }
     else if(nome === ""){
         alert("O campo Nome não foi preenchido!")
+        e.preventDefault()
     }
     else if(genero === "Selecione"){
         alert("O campo Gênero não foi escolhido!")
+        e.preventDefault()
     }
     else if(idade === ""){
         alert("O campo Idade não foi preenchido!")
+        e.preventDefault()
     }
     else if(numero === ""){
         alert("O campo Número não foi preenchido!")
+        e.preventDefault()
     }
     else if(plano === "Selecione"){
         alert("O campo Plano não foi escolhido!")
+        e.preventDefault()
     }
     else{
         var url = "http://wa.me/" + telefone + "?text="
-        + "*FORMULÁRIO CROSS ARENA*" + "%0a"
+        + "*FORMULÁRIO CROSS ARENA*"+"%0a"
         + "*Nome :* " +nome+"%0a"
         + "*Gênero :* " +genero+"%0a"
         + "*Idade :* " +idade+ "%0a"
@@ -50,6 +57,4 @@ function enviar(){
         + "..."
         window.open(url).focus();
     }
-
-    
 }
